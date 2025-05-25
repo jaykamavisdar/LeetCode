382e2bef0d120620db83ff0ba05bee1f211cf4e0
Jay Kamavisdar <kamavisdarj@gmail.com>
2025-05-25 23:05:09 +0300

    Time: 4 ms (59.70%) | Memory: 40.7 MB (27.04%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,6 +1,23 @@
 class Solution {
+    // private boolean perfectSquare(int x){
+    //     if (x >= 4){
+    //         int tHalf = x/2;
+    //         for (int i = 1; i <= tHalf; i++){
+    //             if (i*i == x){
+    //                 return true;
+    //             }
+    //             else{
+    //                 continue;
+    //             }
+    //         }
+    //     }
+    //     else if ((x==0)||(x==1)){
+    //         return true;
+    //     }
+    //     return false;
+    // }
     public boolean judgeSquareSum(int c) {
-        // if (c == 1 || c == 0) return true;
+        // if (perfectSquare(c)) return true;
         // for (int i = 0; i <= c/2; i++){
         //     for (int j = 0; j <= c/2; j++){
         //         if (Math.pow(i,2)+Math.pow(j,2) == c) return true;
